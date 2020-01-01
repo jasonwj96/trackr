b795cbfdbe4e5f2ae43028fca27a470bab4ecd72
Jason Wedderburn <jasonwj96@gmail.com>
2020-01-01 11:18:52 -0500

    Fixed a styling bug

--- src/app/components/navmenu/navmenu.component.ts
+++ src/app/components/navmenu/navmenu.component.ts
@@ -8,5 +8,32 @@ import { Component, OnInit } from "@angular/core";
 export class NavmenuComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    let path = location.pathname.split("/").pop();
+    path = path.substring(0, 1).toUpperCase() + path.substring(1);
+    const buttons = Array.from(document.getElementsByClassName("navmenu-btn"));
+
+    if (path === "") {
+      buttons[0].classList.add("selected");
+    } else {
+      buttons.forEach(button => {
+        Array.from(button.childNodes).forEach(node => {
+          if (node.textContent === path) {
+            button.classList.add("selected");
+          }
+        });
+      });
+    }
+  }
+
+  selectButton(event: any) {
+    const selectedBtn = event.target;
+    const buttons = Array.from(document.getElementsByClassName("navmenu-btn"));
+
+    buttons.forEach(button => {
+      button.classList.remove("selected");
+    });
+
+    selectedBtn.classList.add("selected");
+  }
 }
